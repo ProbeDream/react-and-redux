88ccbbf6cc7f46533462102e81476ae0ef013513
ProbeDream <Gump1016@163.com>
2019-12-15 22:40:58 +0800

    添加对应的章节目录
    代码章节目录一一对应!

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ControlPanel from "../02example/ControlPanel";
+import ControlPanel from "../chapter02/ControlPanel";
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(<ControlPanel />, rootElement);
